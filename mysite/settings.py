6c3efe59ab3aa24f54ae036cb72383e1b6aa7abd
Kerry Nasidai <kerrynasidai@gmail.com>
2016-11-26 17:20:10 +0300

    allowed host

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'da65e@+4d*@9y*z7rvfkmvx0(m6342&e2(tj98ah!6c-32vkhd'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["Knasidai.pythonanywhere.com",]
 
 
 # Application definition
